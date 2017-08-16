b3cc20aa675155bef230cf9ca95a9cfa6e8a15a2
luciobosco <lucio.bosco@gmail.com>
2017-08-16 10:46:04 -0300

    Flujo navegable dinámico
    Agregué un script para hacer navegable el flujo sin tener que repetir pantallas iguales solo por ser de diferente flujo.
    Modifiqué los links, en base al script.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,27 @@
+///// FUNCION UTILIZADA PARA DINAMIZAR EL FLUJO /////
+///// No tener en cuenta para el proyecto final /////
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+}
+
+function searchToObject() {
+  var pairs = window.location.search.substring(1).split("&"),
+    obj = {},
+    pair,
+    i;
+
+  for ( i in pairs ) {
+    if ( pairs[i] === "" ) continue;
+
+    pair = pairs[i].split("=");
+    obj[ decodeURIComponent( pair[0] ) ] = decodeURIComponent( pair[1] );
+  }
+
+  return obj;
+}
+
 $(document).ready(function(){
 	////////////////////////////////////////////////////
 	///////////////// SLIDER COLUMNAS //////////////////
@@ -78,16 +102,16 @@ $(document).ready(function(){
 	  $(this).addClass("jello");
 	});
 
-	//MOSTRAR CHEQUE POR CHEQUE FLUJO SIN TAREJTA
+	//MOSTRAR CHEQUE POR CHEQUE - FLUJO SIN TAREJTA
 	// OJO!! Está hardcodeado para 4 cheques y para el flujo ST (sin tarjeta)
 	var total_cheques_st = 4;
 	var cheque_actual_st = 1;
 	$("body" ).on("click", ".siguiente-cheque-ST", function() {
 		if (cheque_actual_st < total_cheques_st){
 			cheque_actual_st++;
-			$(".clip").attr("src", "img/cheque"+cheque_actual_st+".jpg");
+			$(".clip").attr("src", "img/cheque"+cheque_actual_st+".bmp");
 			if (cheque_actual_st == total_cheques_st){
-				$(".siguiente-cheque-ST").replaceWith("<a href='depositar-monto-cheques-ST.html' class='btn btn-derecha btn-verde'><p>Aceptar</p></a>");
+				$(".siguiente-cheque-ST").replaceWith("<a href='depositar-monto-cheques.html?flujo=ST' class='btn btn-derecha btn-verde'><p>Aceptar</p></a>");
 				$(".siguiente-cheque-ST").delay(000).queue(function(){
 	  				$(this).addClass("pulse");
 				});
@@ -95,21 +119,25 @@ $(document).ready(function(){
 		} 
 	});
 
-	//MOSTRAR CHEQUE POR CHEQUE FLUJO SIN TAREJTA
+	//MOSTRAR CHEQUE POR CHEQUE - FLUJO SIN TAREJTA
 	// OJO!! Está hardcodeado para 4 cheques y para el flujo CT (con tarjeta)
 	var total_cheques_ct = 4;
 	var cheque_actual_ct = 1;
 	$("body" ).on("click", ".siguiente-cheque-CT", function() {
 		if (cheque_actual_ct < total_cheques_ct){
 			cheque_actual_ct++;
-			$(".clip").attr("src", "img/cheque"+cheque_actual_ct+".jpg");
+			$(".clip").attr("src", "img/cheque"+cheque_actual_ct+".bmp");
 			if (cheque_actual_ct == total_cheques_ct){
-				$(".siguiente-cheque-CT").replaceWith("<a href='depositar-monto-cheques-CT.html' class='btn btn-derecha btn-verde'><p>Aceptar</p></a>");
+				$(".siguiente-cheque-CT").replaceWith("<a href='depositar-monto-cheques.html?flujo=CT' class='btn btn-derecha btn-verde'><p>Aceptar</p></a>");
 				$(".siguiente-cheque-CT").delay(000).queue(function(){
 	  				$(this).addClass("pulse");
 				});
 			}
 		} 
 	});
+
+
+
+
 
 });
